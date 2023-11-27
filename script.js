6e07f3578a442ad6d8d3100981d242f0ac09007d
malik233222 <131356290+malik233222@users.noreply.github.com>
2023-11-27 19:21:53 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let start = prompt('1ci reqem daxil edin');
+
+while(+start<10){
+    start++
+    if(start%2==0){
+        document.write(start+ "cut" +`<br>`)
+    }
+    else{
+        document.write(start+ "tek" +`<br>`)
+
+    }
+}
+    
